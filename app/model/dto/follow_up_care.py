f96ca8bee7ec49b7c7b43ece104d9fef06851055
ppijbb <ppijbb@naver.com>
2025-02-04 15:46:06 +0900

    Enhancement: Chatting 진행을 위한  Request parameter 추가

--- app/model/dto/follow_up_care.py
+++ app/model/dto/follow_up_care.py
@@ -2,10 +2,13 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class ChatRequest(BaseModel):
+    uid: str = Field(...)
+    state: int = Field(...)
     text: str = Field(...)
 
 class ChatResponse(BaseModel):
     text: str
+    progress:str = Field(default="chat")
 
     class Config:
         from_attributes = True
--- app/model/dto/medical_inquiry.py
+++ app/model/dto/medical_inquiry.py
@@ -2,6 +2,8 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class ChatRequest(BaseModel):
+    uid: str = Field(...)
+    state: int = Field(...)
     text: str = Field(...)
 
 class ChatResponse(BaseModel):
