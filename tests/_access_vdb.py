3782c8a1d089e131b9ea8aed299c86c0ca6743ee
ppijbb <ppijbb@naver.com>
2025-03-13 17:24:43 +0900

    💪 Enhancement: 한국어 질문 추가 및 VectorStore 기능 확장
    - 시스템 프롬프트의 예시 문구 수정
    - LocalQdrant 클래스 추가 및 QdrantClient 초기화
    - 증상 관련 질문을 VectorStore에 추가

--- tests/_access_vdb.py
+++ tests/_access_vdb.py
@@ -1,4 +1,71 @@
+from qdrant_client import QdrantClient
+from qdrant_client.http import models
 from app.core.langchain_module.rag import VectorStore
 
-vdb = VectorStore(collection_name="state_map")
-print(vdb.get_collection_info())
+class LocalQdrant(VectorStore):
+    
+    def _init_client(self):
+        return QdrantClient(host="localhost", port=6333)
+
+   
+lq = LocalQdrant(collection_name="state_control")
+
+print(lq.get_collection_info())
+
+
+lq.add_text(
+    text="어떤 증상이 있으신가요?", 
+    collection_name="state_control", 
+    metadata={
+        "state": 2,
+        "category_ko": "증상",
+        "category_en": "Symptoms"
+        }
+    )
+lq.add_text(
+    text="증상이 얼마나 지속되었나요?", 
+    collection_name="state_control", 
+    metadata={
+        "state": 2,
+        "category_ko": "증상",
+        "category_en": "Durations"
+        }
+    )
+lq.add_text(
+    text="증상 부위가 어디인지 말씀해 주실 수 있나요?", 
+    collection_name="state_control", 
+    metadata={
+        "state": 1,
+        "category_ko": "증상",
+        "category_en": "Symptoms Area"
+        }
+    )
+lq.add_text(
+    text="통증이 어떤 상황에서 더 심해지나요?", 
+    collection_name="state_control", 
+    metadata={
+        "state": 2,
+        "category_ko": "증상",
+        "category_en": "Severity"
+        }
+    )
+lq.add_text(
+    text="어떤 상황에서 증상이 더 심해지시나요?", 
+    collection_name="state_control", 
+    metadata={
+        "state": 2,
+        "category_ko": "증상 유발요인",
+        "category_en": "Specific Situations"
+        }
+    )
+lq.add_text(
+    text="하고 싶은 말이 있으신가요?", 
+    collection_name="state_control", 
+    metadata={
+        "state": 2,
+        "category_ko": "하고 싶은 말",
+        "category_en": "Special Considerations"
+        }
+    )
+
+
