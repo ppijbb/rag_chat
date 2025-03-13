e681c6655384709ed8144c7f3fd16218b6b440b4
ppijbb <ppijbb@naver.com>
2025-03-13 22:02:47 +0900

    💪 Enhancement: qeury search 추가

--- tests/_access_vdb.py
+++ tests/_access_vdb.py
@@ -12,60 +12,64 @@ lq = LocalQdrant(collection_name="state_control")
 
 print(lq.get_collection_info())
 
+print(lq.search(
+    query="얼마나 아프신가요?", 
+    collection_name="state_control",
+    limit=1))
 
-lq.add_text(
-    text="어떤 증상이 있으신가요?", 
-    collection_name="state_control", 
-    metadata={
-        "state": 2,
-        "category_ko": "증상",
-        "category_en": "Symptoms"
-        }
-    )
-lq.add_text(
-    text="증상이 얼마나 지속되었나요?", 
-    collection_name="state_control", 
-    metadata={
-        "state": 2,
-        "category_ko": "증상",
-        "category_en": "Durations"
-        }
-    )
-lq.add_text(
-    text="증상 부위가 어디인지 말씀해 주실 수 있나요?", 
-    collection_name="state_control", 
-    metadata={
-        "state": 1,
-        "category_ko": "증상",
-        "category_en": "Symptoms Area"
-        }
-    )
-lq.add_text(
-    text="증상의 강도가 얼마나 심하신가요요?", 
-    collection_name="state_control", 
-    metadata={
-        "state": 2,
-        "category_ko": "증상",
-        "category_en": "Severity"
-        }
-    )
-lq.add_text(
-    text="어떤 상황에서 증상이 더 심해지시나요?", 
-    collection_name="state_control", 
-    metadata={
-        "state": 2,
-        "category_ko": "증상 유발요인",
-        "category_en": "Specific Situations"
-        }
-    )
-lq.add_text(
-    text="하고 싶은 말이 있으신가요?", 
-    collection_name="state_control", 
-    metadata={
-        "state": 2,
-        "category_ko": "하고 싶은 말",
-        "category_en": "Special Considerations"
-        }
-    )
+# lq.add_text(
+#     text="어떤 증상이 있으신가요?", 
+#     collection_name="state_control", 
+#     metadata={
+#         "state": 2,
+#         "category_ko": "증상",
+#         "category_en": "Symptoms"
+#         }
+#     )
+# lq.add_text(
+#     text="증상이 얼마나 지속되었나요?", 
+#     collection_name="state_control", 
+#     metadata={
+#         "state": 2,
+#         "category_ko": "증상",
+#         "category_en": "Durations"
+#         }
+#     )
+# lq.add_text(
+#     text="증상 부위가 어디인지 말씀해 주실 수 있나요?", 
+#     collection_name="state_control", 
+#     metadata={
+#         "state": 1,
+#         "category_ko": "증상",
+#         "category_en": "Symptoms Area"
+#         }
+#     )
+# lq.add_text(
+#     text="증상의 강도가 얼마나 심하신가요?", 
+#     collection_name="state_control", 
+#     metadata={
+#         "state": 2,
+#         "category_ko": "증상",
+#         "category_en": "Severity"
+#         }
+#     )
+# lq.add_text(
+#     text="어떤 상황에서 증상이 더 심해지시나요?", 
+#     collection_name="state_control", 
+#     metadata={
+#         "state": 2,
+#         "category_ko": "증상 유발요인",
+#         "category_en": "Specific Situations"
+#         }
+#     )
+# lq.add_text(
+#     text="하고 싶은 말이 있으신가요?", 
+#     collection_name="state_control", 
+#     metadata={
+#         "state": 2,
+#         "category_ko": "하고 싶은 말",
+#         "category_en": "Special Considerations"
+#         }
+#     )
 
 
